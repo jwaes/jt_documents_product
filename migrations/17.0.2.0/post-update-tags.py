863eabac312cff5760e8370c5f9588884492d43c
jwaes <jaco.waes@gmail.com>
2024-07-15 11:01:27 +0000

    asdf

--- migrations/17.0.2.0/post-update-tags.py
+++ migrations/17.0.2.0/post-update-tags.py
@@ -9,20 +9,23 @@ def migrate(cr, version):
     
     po_tag = util.env(cr).user.company_id.product_document_tag_po
     _logger.info("PO tag is %s ", po_tag.name)
-    so_tag = util.env(cr).user.company_id.product_document_tag_so
-    _logger.info("SO tag is %s ", so_tag.name)    
-
-    attach_ids = util.env(cr)['documents.document'].search([['tag_ids', 'in', [po_tag.id, so_tag.id]]]).attachment_id.ids
-
+    attach_ids = util.env(cr)['documents.document'].search([['tag_ids', 'in', po_tag.id]]).attachment_id.ids
     ids = util.env(cr)['product.document'].search([['ir_attachment_id', 'in', attach_ids ]]).ids
 
     ProductDocument = util.env(cr)['product.document']
+
     for record in util.iter_browse(ProductDocument, ids):
-        _logger.info("Product document %s", record.name)
-        if po_tag in record.tag_ids:
-            _logger.info("PO tag")
-            record.mail_attach_on_po = True
-        if so_tag in record.tag_ids:
-            record.mail_attach_on_so = True
-            record.attached_on = 'quotation'
-            _logger.info("SO tag")
+        _logger.info("Product document PO %s", record.name)
+        record.mail_attach_on_po = True
+
+
+    so_tag = util.env(cr).user.company_id.product_document_tag_so
+    _logger.info("SO tag is %s ", so_tag.name)    
+    attach_ids = util.env(cr)['documents.document'].search([['tag_ids', 'in', so_tag.id]]).attachment_id.ids
+    ids = util.env(cr)['product.document'].search([['ir_attachment_id', 'in', attach_ids ]]).ids
+
+    for record in util.iter_browse(ProductDocument, ids):
+        _logger.info("Product document SO %s", record.name)
+        record.mail_attach_on_so = True
+        record.attached_on = 'quotation'
+
