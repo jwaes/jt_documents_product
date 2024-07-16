4d7510f0b7dbdcf0342e233eb89cfa864f94b860
jwaes <jaco.waes@gmail.com>
2024-07-16 06:53:41 +0000

    asdf

--- models/sale_order.py
+++ models/sale_order.py
@@ -38,12 +38,11 @@ class SaleOrder(models.Model):
             docs = order.order_line.product_id.product_document_ids.filtered(lambda s: s.mail_attach_on_so == True)
             _logger.info("docs are %s", len(docs))
             order_result.setdefault('attachment_ids', [])
-            # order_result.setdefault('attachments', []).extend([( docs.ir_attachment_id.name,  docs.ir_attachment_id.datas)])
-            attachments = []
+            attachments = order_result.setdefault('attachments', [])
             for doc in docs:
-                attachments.append([( doc.ir_attachment_id.name,  doc.ir_attachment_id.datas)])
+                 attachments.extend([( doc.ir_attachment_id.name,  doc.ir_attachment_id.datas)])
 
-            order_result['attachments'] = attachments
+            # order_result['attachments'] = attachments
             # return {'attachments': [( dosc.ir_attachment_id.name,  docs.ir_attachment_id.datas)]}
             # for doc in docs:          
             #     order_result.append({ doc.ir_attachment_id.name : doc.ir_attachment_id.datas })        
