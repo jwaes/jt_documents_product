ab1c068b92209e1a2bd45b14f5a2debd7d61c1c9
jwaes <jaco.waes@gmail.com>
2025-02-06 15:17:04 +0000

    post only

--- migrations/18.0.1.0.0/post-update-attachedonsale.py
+++ migrations/18.0.1.0.0/post-update-attachedonsale.py
@@ -6,5 +6,14 @@ _logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
+
+    env = util.env(cr)
+
+    documents = env["product.document"].search([])
+
+    for doc in documents:
+        if doc.mail_attach_on_so:
+            doc.attached_on_sale = 'quotation'
+
             
     util.field.remove_field(cr, "product.document", "mail_attach_on_so")
