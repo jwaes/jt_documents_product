ddf473d2487778d85c03c53939f9c907f093ea10
jwaes <jaco.waes@gmail.com>
2025-02-06 13:10:01 +0000

    attach on sale

---
+++ migrations/18.0.1.0.0/pre-update-attachedonsale.py
@@ -0,0 +1,13 @@
+import logging
+from odoo.upgrade import util
+
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+
+    for record in env['product.document'].browse(ids):
+        
+        if record.mail_attach_on_so:
+            record.attached_on_sale = 'quotation'
